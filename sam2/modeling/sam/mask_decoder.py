1bcfe24222c4873db3a4aca14620680afcd3d42e
Yifeng Zhu <yifeng.zhu@utexas.edu>
2025-03-28 18:32:56 -0500

    import changes to work with orion

--- sam2/modeling/sam/mask_decoder.py
+++ sam2/modeling/sam/mask_decoder.py
@@ -200,7 +200,7 @@ class MaskDecoder(nn.Module):
         if repeat_image:
             src = torch.repeat_interleave(image_embeddings, tokens.shape[0], dim=0)
         else:
-            assert image_embeddings.shape[0] == tokens.shape[0]
+            assert image_embeddings.shape[0] == tokens.shape[0], f"{image_embeddings.shape[0]} != {tokens.shape[0]}"
             src = image_embeddings
         src = src + dense_prompt_embeddings
         assert (
